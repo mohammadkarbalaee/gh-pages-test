a1a5337c4e5cf6364403f71b63763411e92d9f9a
Muhammad Karbalaee <77221151+muhammadksht@users.noreply.github.com>
2022-07-28 08:57:11 +0430

    Updates

---
+++ precache-manifest.68afaee45ad65e06a36351b8efa001a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "681ce063d5e4c1310b76",
+    "url": "/okr/static/css/main.1a61ea6c.chunk.css"
+  },
+  {
+    "revision": "681ce063d5e4c1310b76",
+    "url": "/okr/static/js/main.681ce063.chunk.js"
+  },
+  {
+    "revision": "2d3efeb71c7eebfe7cd0",
+    "url": "/okr/static/js/1.2d3efeb7.chunk.js"
+  },
+  {
+    "revision": "5d9a1f77cbe49a82dc13",
+    "url": "/okr/static/css/2.9105666f.chunk.css"
+  },
+  {
+    "revision": "5d9a1f77cbe49a82dc13",
+    "url": "/okr/static/js/2.5d9a1f77.chunk.js"
+  },
+  {
+    "revision": "6afcd2471471d6a2e675",
+    "url": "/okr/static/js/runtime~main.6afcd247.js"
+  },
+  {
+    "revision": "1f24ff5956f81124bbf9211ad43d1620",
+    "url": "/okr/index.html"
+  }
+];
